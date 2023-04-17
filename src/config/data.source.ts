66edc15222a66b6fcbffe764f655ae9afff49253
Jack Najarro <jacknajarro1@gmail.com>
2023-04-17 00:13:18 -0500

    nts

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -7,6 +7,7 @@ dotenv.config({
   path: process.env.NODE_ENV !== undefined ? `.${process.env.NODE_ENV.trim()}.env` : '.env'
 })
 console.log(process.env.NODE_ENV)
+console.log(`${path.join(__dirname)}/`)
 const Config: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
